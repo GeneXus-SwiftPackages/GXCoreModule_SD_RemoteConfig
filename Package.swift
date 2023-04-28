cffeeb98a4945416e8969ad950fde8254910726d
genexusbot <builder@genexus.com>
2023-04-28 12:44:04 -0300

    Package version: 1.0.0-beta+20230428153652

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_RemoteConfig",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_RemoteConfig-1.0.0-beta+20230428131619.xcframework.zip",
-			checksum: "fb9004ad4d458f85fad9d48395cb81a1cbfdc7eb3f75237b4cd1e660e3f38f7e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_RemoteConfig-1.0.0-beta+20230428153652.xcframework.zip",
+			checksum: "c0bd9a5b1d9be08948f791ffb2a3d7ff249b579c6a44b3b17bd7e108b30cd276"
 		)
 	]
 )
